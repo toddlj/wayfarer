ffd901428e37add4d197eb2bb3c29623e9b39c72
Todd Liebenschutz-Jones <dev@toddljones.me>
2025-02-05 18:52:42 +0000

    Refactor. (#6)

--- src/main/java/com/toddljones/wayfarer/Main.java
+++ src/main/java/com/toddljones/wayfarer/Main.java
@@ -1,7 +1,6 @@
 package com.toddljones.wayfarer;
 
 import java.io.File;
-import java.time.Duration;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,15 +8,13 @@ public class Main {
         MapsRoutingService routingService = new MapsRoutingService(new MapsRoutingService.RoutesClientProvider());
         TelegramNotifier telegramNotifier = new TelegramNotifier();
         ConfigurationParser configurationParser = new ConfigurationParser();
+        RuleEvaluator ruleEvaluator = new RuleEvaluator(telegramNotifier, routingService);
 
         // Configuration
         Configuration configuration = configurationParser.parse(new File("config.yaml"));
 
         // Execution
         Configuration.Rule rule = configuration.rules().getFirst();
-        java.time.Duration duration = routingService.fetchCurrentTransitTimeBetween(rule.origin(), rule.destination());
-        if (duration.compareTo(Duration.ofMinutes(rule.travelTime().notificationThresholdMinutes())) > 0) {
-            telegramNotifier.sendMessage(rule.user().telegramUserId(), "Travel time from %s to %s is greater than %s minutes. It is currently %s minutes".formatted(rule.origin().name(), rule.destination().name(), rule.travelTime().notificationThresholdMinutes(), duration.toMinutes()));
-        }
+        ruleEvaluator.evaluateRule(rule);
     }
 }
